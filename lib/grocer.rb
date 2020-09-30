e70cd3e58f563bf969306945247a2e259f18f542
Alyssa Easterly <alyssa.easterly@yahoo.com>
2020-09-30 22:18:45 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -20,6 +20,7 @@ def consolidate_cart(cart)
   #
   # REMEMBER: This returns a new Array that represents the cart. Don't merely
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
+  #
   binding.pry
   updated_cart = []
   
