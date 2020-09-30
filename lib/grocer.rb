94f506d85e10c773de93689796d17056b452623c
Alyssa Easterly <alyssa.easterly@yahoo.com>
2020-09-30 22:52:48 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -31,8 +31,9 @@ def consolidate_cart(cart)
   else
     new_cart_item ={
       :item => cart[counter][:item],
-      :price => cart[counter][:item],
-    
+      :price => cart[counter][:price],
+      :clearance => cart[counter][:clearance],
+      :count 
     }
   end
   count += 1
